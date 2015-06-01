59213d2dbf4e2382f6d0b1f4d8d0f8eeda966386
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2015-06-01 15:55:16 -0400

    Basic props for reactjs done

--- react/app.js
+++ react/app.js
@@ -1,24 +1,10 @@
 var HelloUser = React.createClass({displayName: "HelloUser",
-//getInitialState method is “The way in which you set the state of a component”.
-// In other terms, getInitialState returns an object which contains the state or data for our component.
-  getInitialState: function(){
-    return {
-      username: '@sanjay1909'
-    }
-  },
-  handleChange: function(e){
-    this.setState({
-      username: e.target.value
-    });
-  },
   render: function(){
-     return (
-      React.createElement("div", null,
-        "Hello ", this.state.username, " ", React.createElement("br", null),
-        "Change Name: ", React.createElement("input", {type: "text", value: this.state.username, onChange: this.handleChange})
-      )
+    return (
+      React.createElement("div", null, " Hello, ", this.props.name)
     )
   }
 });
 
-React.render(React.createElement(HelloUser, null) , document.getElementById('app'));
+React.render(React.createElement(HelloUser, {name: "Sanjay"}), document.getElementById('app'));
+
--- react/app.jsx
+++ react/app.jsx
@@ -1,26 +1,10 @@
 var HelloUser = React.createClass({
-//getInitialState method is “The way in which you set the state of a component”.
-// In other terms, getInitialState returns an object which contains the state or data for our component.
-  getInitialState: function(){
-    return {
-      username: '@sanjay1909'
-    }
-  },
-  handleChange: function(e){
-  // whenever setState is called, React creates a new virtual DOM, does the diff, then updates the real DOM.
-    this.setState({
-      username: e.target.value
-    });
-  },
   render: function(){
-     return (
-      <div>
-        Hello {this.state.username} <br />
-        //onChange is a React thing and it will call whatever method you specify every time the value in the input box changes,
-        Change Name: <input type="text" value={this.state.username} onChange={this.handleChange} />
-      </div>
+    return (
+      <div> Hello, {this.props.name}</div>
     )
   }
 });
 
-React.render(<HelloUser/> , document.getElementById('app'));
+React.render(<HelloUser name="Sanjay"/>, document.getElementById('app'));
+
