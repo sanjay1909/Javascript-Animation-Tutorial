9ca100e5bdac87c4fecb8daeef52d323a093998b
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2015-06-01 14:43:40 -0400

    hello - world first react code done

--- react/app.jsx
+++ react/app.jsx
@@ -1,4 +1,8 @@
- var HelloWorld = React.createClass({
+//Every component is required to have a render method, render is essentially the template for our component.
+//React.render takes in 2 arguments.
+//The first argument is the component  to render,
+//the second argument is the DOM node where we want to render the component.
+var HelloWorld = React.createClass({
              render: function () {
                         return  < div > Hello World!< /div>
                     }
