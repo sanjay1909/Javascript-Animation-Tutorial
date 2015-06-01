829657f70e666b2a3b7f46d13a16c2d7c54b9cd1
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2015-06-01 17:21:39 -0400

    Interactive Props example Done

--- react/app.js
+++ react/app.js
@@ -1,3 +1,4 @@
+//parent component
 var FriendsContainer = React.createClass({displayName: "FriendsContainer",
   getInitialState: function(){
     return {
@@ -5,16 +6,51 @@ var FriendsContainer = React.createClass({displayName: "FriendsContainer",
       friends: ['Krishna', 'Vignesh', 'Raj']
     }
   },
+  addFriend: function(friend){
+    this.setState({
+      friends: this.state.friends.concat([friend])
+    });
+  },
   render: function(){
     return (
       React.createElement("div", null,
         React.createElement("h3", null, " Name: ", this.state.name, " "),
+        React.createElement(AddFriend, {addNew: this.addFriend}),
         React.createElement(ShowList, {names: this.state.friends})
       )
     )
   }
 });
 
+var AddFriend = React.createClass({displayName: "AddFriend",
+  getInitialState: function(){
+    return {
+      newFriend: ''
+    }
+  },
+  updateNewFriend: function(e){
+    this.setState({
+      newFriend: e.target.value
+    });
+  },
+  handleAddNew: function(){
+    this.props.addNew(this.state.newFriend);
+    this.setState({
+      newFriend: ''
+    });
+  },
+  render: function(){
+    return (
+        React.createElement("div", null,
+          React.createElement("input", {type: "text", value: this.state.newFriend, onChange: this.updateNewFriend}),
+          React.createElement("button", {onClick: this.handleAddNew}, " Add Friend ")
+        )
+    );
+  }
+});
+
+// child component
+// props.names is got from UI attribute to Component
 var ShowList = React.createClass({displayName: "ShowList",
   render: function(){
     var listItems = this.props.names.map(function(friend){
--- react/app.jsx
+++ react/app.jsx
@@ -6,17 +6,50 @@ var FriendsContainer = React.createClass({
       friends: ['Krishna', 'Vignesh', 'Raj']
     }
   },
+  addFriend: function(friend){
+    this.setState({
+      friends: this.state.friends.concat([friend])
+    });
+  },
   render: function(){
     return (
       <div>
         <h3> Name: {this.state.name} </h3>
+        <AddFriend addNew={this.addFriend} />
         <ShowList names={this.state.friends} />
       </div>
     )
   }
 });
 
-//child component
+var AddFriend = React.createClass({
+  getInitialState: function(){
+    return {
+      newFriend: ''
+    }
+  },
+  updateNewFriend: function(e){
+    this.setState({
+      newFriend: e.target.value
+    });
+  },
+  handleAddNew: function(){
+    this.props.addNew(this.state.newFriend);
+    this.setState({
+      newFriend: ''
+    });
+  },
+  render: function(){
+    return (
+        <div>
+          <input type="text" value={this.state.newFriend} onChange={this.updateNewFriend} />
+          <button onClick={this.handleAddNew}> Add Friend </button>
+        </div>
+    );
+  }
+});
+
+// child component
 // props.names is got from UI attribute to Component
 var ShowList = React.createClass({
   render: function(){
