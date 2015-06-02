74c15b9f128dbf9b1eeb409fce516ca704c83486
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2015-06-02 14:30:26 -0400

    propType and getDefaultProps done for reactJS

--- react/app.jsx
+++ react/app.jsx
@@ -1,71 +1,76 @@
 //parent component
 var FriendsContainer = React.createClass({
-  getInitialState: function(){
-    return {
-      name: 'Sanjay',
-      friends: ['Krishna', 'Vignesh', 'Raj']
+    getInitialState: function () {
+        return {
+            name: 'Sanjay',
+            friends: ['Krishna', 'Vignesh', 'Raj']
+        }
+    },
+    addFriend: function (friend) {
+        this.setState({
+            friends: this.state.friends.concat([friend])
+        });
+    },
+    render: function () {
+        return ( <div>
+                    <h3> Name: {this.state.name} </h3>
+                    <AddFriend addNew = {this.addFriend}/>
+                    <ShowList names = {this.state.friends}/>
+                </div>
+            )
     }
-  },
-  addFriend: function(friend){
-    this.setState({
-      friends: this.state.friends.concat([friend])
-    });
-  },
-  render: function(){
-    return (
-      <div>
-        <h3> Name: {this.state.name} </h3>
-        <AddFriend addNew={this.addFriend} />
-        <ShowList names={this.state.friends} />
-      </div>
-    )
-  }
 });
 
+//proptype addnew is a function
+//props can be considered as attributes of a tag
+// state is object public properties.
 var AddFriend = React.createClass({
-  getInitialState: function(){
-    return {
-      newFriend: ''
-    }
-  },
-  updateNewFriend: function(e){
-    this.setState({
-      newFriend: e.target.value
-    });
-  },
-  handleAddNew: function(){
-    this.props.addNew(this.state.newFriend);
-    this.setState({
-      newFriend: ''
-    });
-  },
-  render: function(){
-    return (
-        <div>
-          <input type="text" value={this.state.newFriend} onChange={this.updateNewFriend} />
-          <button onClick={this.handleAddNew}> Add Friend </button>
-        </div>
-    );
-  }
+    getInitialState: function () {
+        return {
+            newFriend: ''
+        }
+    },
+    propTypes: {
+        addNew: React.PropTypes.func.isRequired
+    },
+    updateNewFriend: function (e) {
+        this.setState({
+            newFriend: e.target.value
+        });
+    },
+    handleAddNew: function () {
+        this.props.addNew(this.state.newFriend);
+        this.setState({
+            newFriend: ''
+        });
+    },
+    render: function () {
+        return ( <div>
+                    <input type = "text" value = {this.state.newFriend} onChange = {this.updateNewFriend}/>
+                    <button onClick = {this.handleAddNew} > Add Friend </button>
+                </div>
+                );
+        }
 });
 
 // child component
 // props.names is got from UI attribute to Component
 var ShowList = React.createClass({
-  render: function(){
-    var listItems = this.props.names.map(function(friend){
-      return <li> {friend} </li>;
-    });
-    return (
-      <div>
-        <h3> Friends </h3>
-        <ul>
-          {listItems}
-        </ul>
-      </div>
-    )
-  }
+    getDefaultProps: function () {
+        return {
+            names: []
+        }
+    },
+    render: function () {
+        var listItems = this.props.names.map(function (friend) {
+            return <li> {friend} </li>;
+        });
+        return ( <div>
+                    <h3> Friends </h3>
+                    <ul> {listItems} </ul>
+                </div>
+                )
+        }
 });
 
-React.render(<FriendsContainer/>, document.getElementById('app'));
-
+React.render( <FriendsContainer/> , document.getElementById('app'));
